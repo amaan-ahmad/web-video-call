c5e0a36f0f542faf0f60771eedddb5ed96f9865c
Amaan Ahmad <amaanahmad582001@gmail.com>
2021-02-27 18:17:50 +0530

    peer connection user id

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-const SERVER_PORT = process.env.SERVER_PORT || 5000; 
+const SERVER_PORT = process.env.SERVER_PORT || 5000;
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
-const ejs = require('ejs');
-const { v4: uuidV4 } = require('uuid');
+const ejs = require("ejs");
+const { v4: uuidV4 } = require("uuid");
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.redirect(`/${uuidV4()}`);
 });
 
-
-app.get("/:room", (req, res)=>{
-  res.render('room', {
-    roomID: req.params.room
-  })
+app.get("/:room", (req, res) => {
+  res.render("room", {
+    roomID: req.params.room,
+  });
 });
 
-io.on("connection", socket => {
-  socket.on("join-room", (roomID, userID)=>{
-    console.log(roomID, userID);
-  })
-})
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomID, userID) => {
+    socket.join(roomID);
+    socket.to(roomID).broadcast.emit("user-connected", userID);
+  });
+});
 
 server.listen(SERVER_PORT);
