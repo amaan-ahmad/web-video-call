eecad3b0b88e37149749a566cd75bc9c9d930c74
Amaan Ahmad <amaanahmad582001@gmail.com>
2021-02-28 14:27:07 +0530

    remote stream added, yet to fix

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
-const ejs = require("ejs");
 const { v4: uuidV4 } = require("uuid");
 
-app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.redirect(`/${uuidV4()}`);
@@ -26,6 +25,9 @@ io.on("connection", (socket) => {
   socket.on("join-room", (roomID, userID) => {
     socket.join(roomID);
     socket.to(roomID).broadcast.emit("user-connected", userID);
+    socket.on("disconnect", () => {
+      socket.to(roomID).broadcast.emit("user-disconnected", userID);
+    });
   });
 });
 
